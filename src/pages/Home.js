7292da922ed0d4c3946e79d435012cdb9794a0b4
jordanhudgens <jordan@devcamp.com>
2019-03-09 21:07:50 -0600

    Created a registration component

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>Home goes here!</div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Register.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default class Register extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      registrationError: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    axios
+      .post(
+        "https://bottega-activity-tracker-api.herokuapp.com/registrations",
+        {
+          user: {
+            email: this.state.email,
+            password: this.state.password
+          }
+        }
+      )
+      .then(response => {
+        if (response.data.status === "created") {
+          this.props.history.push("/");
+          this.props.handleSuccessfulRegistration(response.data);
+        } else {
+          this.setState({
+            registrationError: "There was an error creating your account."
+          });
+        }
+      })
+      .catch(error => {
+        console.log("in handle submit error for registrations", error);
+        this.setState({
+          registrationError: "There was an error creating your account."
+        });
+      });
+
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="form-wrapper">
+        {this.state.registrationError ? (
+          <div className="form-error">
+            <FontAwesomeIcon icon="exclamation-circle" />
+            {this.state.registrationError}
+          </div>
+        ) : null}
+        <div className="form-group">
+          <div className="icon-wrapper">
+            <FontAwesomeIcon icon="at" />
+          </div>
+          <input
+            className="full-width-element"
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <div className="icon-wrapper">
+            <FontAwesomeIcon icon="key" />
+          </div>
+          <input
+            className="full-width-element"
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+        </div>
+
+        <div className="login-btn-wrapper">
+          <button className="primary-rounded-button button-small" type="submit">
+            Register
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
