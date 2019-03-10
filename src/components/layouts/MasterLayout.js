8946593834c31243f1f505fdd535869cb2330626
jordanhudgens <jordan@devcamp.com>
2019-03-09 21:48:18 -0600

    Setup the nav system

--- src/components/layouts/MasterLayout.js
+++ src/components/layouts/MasterLayout.js
@@ -1,7 +1,13 @@
 import React from "react";
+import DashboardNavigation from "../navigation/DashboardNavigation";
 
 const MasterLayout = props => {
-  return <div className="layout-wrapper">{props.children}</div>;
+  return (
+    <div className="layout-wrapper">
+      <DashboardNavigation />
+      {props.children}
+    </div>
+  );
 };
 
 export default MasterLayout;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
 
 import MasterLayout from "../components/layouts/MasterLayout";
-import DashboardNavigation from "../components/navigation/DashboardNavigation";
 
 const Home = () => {
   return (
     <MasterLayout>
-      <DashboardNavigation />
       <div className="error-page-container">
         <div className="content">
           <h2>home!!!</h2>
