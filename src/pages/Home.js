7600924adb967abb7a88cab565146b4becc8fb53
jordanhudgens <jordan@devcamp.com>
2019-03-13 00:27:52 -0500

    Implemented animated svg for homepage, about to switch back to non animated version. keep this logged for future usage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import Vivus from "vivus";
-import svg from "../components/svgs/some.svg";
 import homepageSvg from "../components/svgs/homepageSvg.svg";
 
 import MasterLayout from "../components/layouts/MasterLayout";
-import HomepageImage from "../components/svgs/HomepageImage";
 
 export default class Home extends Component {
   constructor() {
