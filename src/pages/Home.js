e6a9173105aeb65e9089a4a8d35e8a5fcc199111
jordanhudgens <jordan@devcamp.com>
2019-03-12 23:50:37 -0500

    Added homepage svgs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
+import Vivus from "vivus";
+import svg from "../components/svgs/some.svg";
+import homepageSvg from "../components/svgs/homepageSvg.svg";
 
 import MasterLayout from "../components/layouts/MasterLayout";
 import HomepageImage from "../components/svgs/HomepageImage";
 
-const Home = () => {
-  return (
-    <MasterLayout>
-      <div className="homepage-container">
-        <div className="content">
-          <div className="left-column">Left column home</div>
-          <div className="right-column">
-            <HomepageImage />
+export default class Home extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    new Vivus("graph", { duration: 200, file: homepageSvg }, null);
+  }
+
+  render() {
+    return (
+      <MasterLayout>
+        <div className="homepage-container">
+          <div className="content">
+            <div className="left-column">Left column home</div>
+            <div className="right-column">
+              <div id="graph" />
+            </div>
           </div>
         </div>
-      </div>
-    </MasterLayout>
-  );
-};
-
-export default Home;
+      </MasterLayout>
+    );
+  }
+}
