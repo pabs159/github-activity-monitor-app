24c764bd50e2be6f87b27adc2d13051c96985f51
jordanhudgens <jordan@devcamp.com>
2019-03-29 18:10:15 -0400

    Installed d3 visualizations and created an account heat map

--- src/pages/dashboard/AccountDetail.js
+++ src/pages/dashboard/AccountDetail.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import StudentHeatMap from "../../components/charts/StudentHeatMap";
 
 export default class AccountDetail extends Component {
   render() {
-    return <div>AccountDetail</div>;
+    return (
+      <div>
+        <StudentHeatMap width={500} height={500} />
+      </div>
+    );
   }
 }
